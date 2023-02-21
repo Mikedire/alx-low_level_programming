2c7d189bed6327c81a6f69553268078b49471ceb
Mikedire <olahjoho@gmail.com>
2023-02-21 12:05:24 -0800

    jjhhhjk

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,15 +7,12 @@
  */
 
 void print_alphabet(void)
-
-int main(void)
 {
 	char i;
 
-	for (i = 'a';i <= 'z'; i++)
+	for(i = 'a'; i <= 'z'; i++)
 	{
 		_putchar(i);
 	}
-		_putchar("\n");
-		return (0);
+	_putchar('\n');
 }
