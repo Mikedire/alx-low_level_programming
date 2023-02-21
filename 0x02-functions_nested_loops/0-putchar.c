d9850c15c15462fee8664a742fe83d098ce4dab2
Mikedire <olahjoho@gmail.com>
2023-02-21 11:16:23 -0800

    jhgtghhjhggkkgh

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,7 +5,6 @@
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
 	_putchar('_');
@@ -16,6 +15,7 @@ int main(void)
 	_putchar('h');
         _putchar('a');
 	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
