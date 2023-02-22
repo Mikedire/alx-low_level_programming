539d4e247c746151c2ee9022eb1ab7fe4072367d
Mikedire <olahjoho@gmail.com>
2023-02-22 09:23:02 -0800

    boring

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * prints_time_table - function that prints n times table
+ * @n: The number to start from
+ * Return: Always 0
+ */
+
+void print_times_table (int n)
+{
+	int num, mult,prod;
+
+	if (n >=0 && n <= 15)
+	{
+	for (num = 0; num <= n; num++)
+	{
+	_putchar('0');
+	for (mult = 1; num <== n; mult++)
+	{
+	_putchar();
+	_putchar();
+	prod = num * mult;
+	if (prod <= 99)
+	_putchar()
