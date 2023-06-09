01b1819847325bf788dc8188d618cb66d582374d
Mikedire <olahjoho@gmail.com>
2023-06-09 13:41:17 -0700

    third commit

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,10 @@
+iinclude "main.h"
+#include <stdio.h>
+
+/**
+ * main - program that prints the minimum number of coins to make change for an amount of money.
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always zero
+ */
